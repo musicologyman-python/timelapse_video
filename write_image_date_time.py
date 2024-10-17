cb47e2f0f4d40a6a4e0b579a47acc4a404968af2
Richard Wattenbarger <richard@wattenbarger.net>
2024-10-17 00:52:38 -0400

    added logging

--- write_image_date_time.py
+++ write_image_date_time.py
@@ -10,8 +10,9 @@ import tkinter.filedialog as fd
 from typing import Callable
 
 from icecream import ic
+from loguru import logger
 from PIL import Image, ImageColor, ImageDraw, ImageFont
-from toolz.functoolz import compose_left, pipe
+from toolz.functoolz import compose_left, curry
 
 VALID_IMAGE_SUFFIXES = ['.jpg', '.jpeg', '.png']
 FONT_PATH = 'System/Library/Fonts/Monaco.ttf'
@@ -29,16 +30,9 @@ SEQ_NUM_OFFSET_FROM_BOTTOM = 10
 TEXT_COLOR = ImageColor.getcolor('white', mode='RGBA')
 VIDEO_DIR: Path = Path('video')
 
-get_date_from_file_name: Callable[[Path], datetime] = (
-    compose_left(
-        attrgetter('stem'),
-        int,
-        datetime.fromtimestamp))
-
-get_image_date_string_from_file_name: Callable[[Path], str] = (
-    compose_left(
-        get_date_from_file_name,
-        methodcaller('strftime', '%Y-%m-%d %H:%M:%S')))
+def get_image_date_string_from_file_name(image_path: Path) -> str:
+    return (datetime.strptime(image_path.stem, '%Y%m%d%H%M%S')
+                    .strftime('%Y-%m-%d %H:%M:%S'))
 
 def get_text_anchor_coordinates(bounding_box: tuple[int, int, int, int]) \
         -> tuple[int, int]:
@@ -47,20 +41,27 @@ def get_text_anchor_coordinates(bounding_box: tuple[int, int, int, int]) \
     left_anchor: int = (IMAGE_WIDTH - text_width) // 2
     return (left_anchor, TIMESTAMP_OFFSET_FROM_TOP)
 
-def label_image(p: Path) -> None:
+@logger.catch
+def label_image(dest_dir: Path, p: Path) -> None:
+    if (dest_file := dest_dir / f'{p.stem}.jpeg').exists():
+        return
     image_date_string = get_image_date_string_from_file_name(p)
     with Image.open(p) as im:
-        draw = ImageDraw.Draw(im)
-        bounding_box: tuple[int, int, int, int] = \
-            TIMESTAMP_FONT.getbbox(text=image_date_string)
-        xy = get_text_anchor_coordinates(bounding_box)
-        draw.text(xy=xy, text=image_date_string, font=TIMESTAMP_FONT)
-        # draw POSIX timestamp
-        _, _, _, bottom = bounding_box
-        posix_timestamp_left, _ = xy
-        draw.text(xy=(posix_timestamp_left, bottom + TIMESTAMP_OFFSET_FROM_TOP), 
-                  text=p.stem, font=POSIX_TIMESTAMP_FONT)
-        im.save(VIDEO_DIR / f'{p.stem}.jpeg')
+        try:
+            draw = ImageDraw.Draw(im)
+            bounding_box: tuple[int, int, int, int] = \
+                TIMESTAMP_FONT.getbbox(text=image_date_string)
+            xy = get_text_anchor_coordinates(bounding_box)
+            draw.text(xy=xy, text=image_date_string, font=TIMESTAMP_FONT)
+            # draw POSIX timestamp
+            _, _, _, bottom = bounding_box
+            posix_timestamp_left, _ = xy
+            draw.text(xy=(posix_timestamp_left, bottom + TIMESTAMP_OFFSET_FROM_TOP), 
+                      text=p.stem, font=POSIX_TIMESTAMP_FONT)
+            im.save(dest_file)
+        except Exception as ex:
+            logger.exception(str(ex))
+            logger.log('DEBUG', f'Exception writing file {p!s}')
 
 def get_image_src_dir() -> Path:
     IMAGE_DIR_TITLE = 'Choose a folder containing the images to be labeled'
@@ -69,10 +70,19 @@ def get_image_src_dir() -> Path:
 def ensure_video_dest_dir_exists() -> None:
     if not VIDEO_DIR.exists():
         VIDEO_DIR.mkdir(parents=True, exist_ok=True)
-
+        
+def create_video_dest_dir(image_dir: Path) -> Path:
+    video_dir_name = f'video_{image_dir.name}'
+    video_dir = Path(video_dir_name)
+    video_dir.mkdir(parents=True, exist_ok=True)
+    return video_dir
+        
 def main():
-    ensure_video_dest_dir_exists()
+    logger.add('file_{time:YYYY-MM-DD HH.mm.ss}.log')
+    # ensure_video_dest_dir_exists()
     image_dir: Path = get_image_src_dir()
+    video_dir: Path = create_video_dest_dir(image_dir)
+
     image_files: Iterable[Path] = (p for p in image_dir.iterdir() 
                                    if p.is_file() 
                                    and not p.is_symlink()
@@ -82,7 +92,7 @@ def main():
         if i % 10 == 0:
             print(f'\rLabeling image {i}', end='')
             stdout.flush()
-        label_image(p)
+        label_image(video_dir, p)
 
     print()
     print('Done')
